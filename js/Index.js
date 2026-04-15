842c0577a4f1f344fb2b0bc4ffe6d27ba602d7d7
Huzan3 <huguitorodriguezvera@gmail.com>
2026-04-15 13:33:27 +0200

    Css de tienda + arreglo js index

--- js/Index.js
+++ js/Index.js
@@ -2,6 +2,7 @@ const inicio = document.getElementById("inicio");
 const texto = document.getElementById("texto");
 const send = document.getElementById("send");
 const usuario = document.getElementById("usuario");
+const contraseña = document.getElementById("contraseña");
 
 let contador = 0;
 inicio.addEventListener("click", function(){
@@ -10,14 +11,19 @@ inicio.addEventListener("click", function(){
         texto.innerHTML = "Register";
         send.innerHTML = "Registrarte";
         usuario.value = "";
+        contraseña.value = "";
         contador++;
     }else{
         inicio.innerHTML = "Register";
         texto.innerHTML = "Login";
         send.innerHTML = "Iniciar sesión";
         usuario.value = "";
+        contraseña.value = "";
         contador--;
     }
+});
 
-
+send.addEventListener("click", function(){
+    usuario.value = "";
+    contraseña.value = "";
 });
