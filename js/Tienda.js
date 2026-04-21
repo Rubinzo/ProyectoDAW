966bcc929a60720656dd479fb4e6dedea7017104
Rubinzo <random.randint.nft@gmail.com>
2026-04-21 13:46:32 +0200

    Camisetas con base de datos conectada

--- js/Tienda.js
+++ js/Tienda.js
@@ -1,30 +1,55 @@
 //Generar tarjetas
 const contenedorProductos = document.getElementById("contenedorProductos");
 
+    
+const myHeaders = new Headers();
+const getCamisetas = {
+                method: "GET",
+                headers: myHeaders,
+                redirect: "follow",
+            };
+fetch("http://localhost:8080/stock/camisetas", getCamisetas)
+                .then((response) => response.json())
+                .then((result) => {
+                    console.log(result);
+                    for(let i = 0; i < result.length; i++ ){
 
-fetch("/Producto.xml").then((response) => response.text())
-    .then((data) => {
-
-        const parser = new DOMParser();
-        const xml = parser.parseFromString(data, "text/xml");
-        const productos = xml.getElementsByTagName("producto");
-
-        for(let producto of productos){
-
-            const nombre = producto.getElementsByTagName("nombre")[0].textContent;
-            const precio = producto.getElementsByTagName("precio")[0].textContent;
+                        const nombre = result[i].nombre;
+                        const precio = result[i].precio;
+                        const img = result[i].img;
 
 
-            contenedorProductos.innerHTML += `
-            <div class="producto" data-nombre = ${nombre} 
-            data-status="activo">
-                <p>${nombre}</p>
-                <p>${precio}</p>
-            </div>
-            `;
+                        contenedorProductos.innerHTML += `
+                        <div class="producto" data-nombre = ${nombre} 
+                        data-status="activo">
+                            <p>${nombre}</p>
+                            <img src="${img}" alt="${nombre}">
+                            <p>${precio}€</p>
+                        </div>
+                        `;
 
-        }
+                    }
+                    const producto = document.getElementsByClassName("producto");
+                    const buscar = document.getElementById("buscar");
+                    //buscar.addEventListener("input", filtrar);
+                    const inputNombre = document.getElementById("inputNombre");
+                    inputNombre.addEventListener("input",filtrar)
+                    function filtrar(){
+                        for(let i = 0; i < result.length; i++){
+                            let nombreInput = result[i].nombre.toLowerCase();
+                            if(nombreInput.includes(inputNombre.value.toLowerCase())){
+                                console.log("coincide")
+                                producto[i].style.display ="block";
+                            
+                            }else{
+                                console.log("No coincide");
+                                producto[i].style.display = "none";
+                                
+                            }
+                        }
+                    }
+                })
+                .catch((error) => console.error(error));
 
-    });
-
+//Buscador por nombre
 
