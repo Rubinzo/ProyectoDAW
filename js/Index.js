346cf6f5bfc26822e0527c3892e6325f63ca02cf
Rubinzo <random.randint.nft@gmail.com>
2026-04-21 10:06:36 +0200

    Register funcional+ mensajes de error

--- js/Index.js
+++ js/Index.js
@@ -2,67 +2,102 @@ const inicio = document.getElementById("inicio");
 const texto = document.getElementById("texto");
 const send = document.getElementById("send");
 const usuario = document.getElementById("usuario");
-const contraseña = document.getElementById("contraseña");
+const contraseña1 = document.getElementById("contraseña1");
+const contraseña2 = document.getElementById("contraseña2");
+const validar = document.getElementById("validar");
 
-let contador = 0;
+
+contraseña2.style.display = "none";
+validar.style.display = "none";
+let login = true;
 inicio.addEventListener("click", function(){
-    if(contador == 0){
+    if(login == true){
         inicio.innerHTML = "Login";
         texto.innerHTML = "Register";
         send.innerHTML = "Registrarte";
         usuario.value = "";
-        contraseña.value = "";
-        contador++;
+        contraseña1.value = "";
+        contraseña2.style.display = "block";
+        validar.style.display = "block";
+        login = false;
     }else{
         inicio.innerHTML = "Register";
         texto.innerHTML = "Login";
         send.innerHTML = "Iniciar sesión";
         usuario.value = "";
-        contraseña.value = "";
-        contador--;
+        contraseña1.value = "";
+        contraseña2.value = "";
+        contraseña2.style.display = "none";
+        validar.style.display = "none";
+        login = true;
     }
 });
 const myHeaders = new Headers();
 
-send.addEventListener("click", registrarse);
-
+send.addEventListener("click", sesion);
+const mensaje = document.getElementById("mensaje");
 //Registrarse
-function registrarse() {
+function sesion() {
     const usuario = document.getElementById("usuario");
     const contraseña1 = document.getElementById("contraseña1");
     const contraseña2 = document.getElementById("contraseña2");
 
     let usuarioValue;
     let contraseña;
-    if (contraseña1.value == contraseña2.value) {
-        usuarioValue = usuario.value;
-        contraseña = contraseña1.value;
-        console.log("Nombre: " , usuarioValue);
-        console.log("Contraseña: " , contraseña);
-        let json = {
-            usuario: usuario.value,
-            contraseña: contraseña1.value
-        };
-        console.log(json)
-        const sendRegistro = {
-            method: "POST",
-            headers: myHeaders,
-            redirect: "follow",
-            body: JSON.stringify(json)
-        };
-        usuario.value = "";
-        contraseña2.value = "";
-        fetch("http://localhost:8080/usuario/register", sendRegistro)
-            .then((response) => response.json())
-            .then((result) => {
-                console.log(result);
-            })
-            .catch((error) => console.error(error));
 
-    } else {
-        console.log("La contraseña tiene que ser igual");
+    if(login == true){
+
+    }else{
+        if (contraseña1.value == contraseña2.value) {
+            usuarioValue = usuario.value;
+            contraseña = contraseña1.value;
+            console.log("Nombre: " , usuarioValue);
+            console.log("Contraseña: " , contraseña);
+            let json = {
+                usuario: usuario.value,
+                contraseña: contraseña1.value
+            };
+            console.log(json)
+            const sendRegistro = {
+                method: "POST",
+                headers: myHeaders,
+                redirect: "follow",
+                body: JSON.stringify(json)
+            };
+            usuario.value = "";
+            contraseña1.value = "";
+            contraseña2.value = "";
+            fetch("http://localhost:8080/user/register", sendRegistro)
+                .then((response) => response.json())
+                .then((result) => {
+                    console.log(result);
+
+                    if(result.registrado == false){
+                        mensaje.innerText = "Usuario registrado correctamente";
+                        mensaje.style.color = "green";
+                        setTimeout(() =>{
+                            window.location.href = "Tienda.html"; 
+                        }, 1500);
+                    }else{
+                        mensaje.innerText ="Ese usuario ya está registrado";
+                        mensaje.style.color = "red";
+                        setTimeout(() =>{
+                            mensaje.innerText ="";
+                        }, 1500);
+                    }
+                })
+                .catch((error) => console.error(error));
+
+        } else {
+            mensaje.innerText ="La contraseña tiene que ser igual";
+            mensaje.style.color = "red";
+            setTimeout(() =>{
+                mensaje.innerText ="";
+            }, 1500);
+        }
     }
 
 
-
 }
+
+
